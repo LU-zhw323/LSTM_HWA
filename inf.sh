4b7b1c4294c26e55afd69aef26a8721c9dcfc302
Zhenyu Wu <wzy2575765661@gmail.com>
2024-05-12 01:29:59 -0400

    update

--- inf.sh
+++ inf.sh
@@ -7,7 +7,7 @@
 #SBATCH -t 2880
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=zhw323@lehigh.edu
-#SBATCH --array=1-25
+#SBATCH --array=1-100
 #SBATCH --output=./output/std_log/myjob-%A-%a.out
 #SBATCH --error=./output/err_log/myjob-%A-%a.err
 
@@ -19,7 +19,7 @@ TASK_TYPES=("inference_noise" "drift" "gmax" "gmin")
 MODEL_TYPES=("FP" "HWA")
 DRIFT_COMPENSATION=("1" "0")
 
-TASK_TYPE=${TASK_TYPES[3]}
+TASK_TYPE=${TASK_TYPES[1]}
 MODEL_TYPE=${MODEL_TYPES[0]}
 USE_COMPENSATION=${DRIFT_COMPENSATION[0]}
 
